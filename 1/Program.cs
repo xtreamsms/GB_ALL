d68bfe12b8039aebbfa77ce6369d85ebc2b2f2dd
«xtream» <xtreamsms@mail.ru>
2022-10-26 11:10:21 +0700

    working first zad

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int i = 5;
+int j = 10;
+int max = 0;
+int min = 0;
+
+if (i > j)
+{
+    max = i;
+    min = j;
+}   
+else
+{
+    max = j;
+    min = i;
+}
+
+Console.WriteLine ("min: " + min + "max: " + max);
