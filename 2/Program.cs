8511d0ba667688920919138d99c0c836e43cc28d
«xtream» <xtreamsms@mail.ru>
2022-10-26 11:50:00 +0700

    VTOROE Zadacha tri 4isla

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 1;
+int j = 2;
+int z = 3;
+
+int max = i;
+
+    if(j > max)
+    {
+        max = j;
+    }
+    
+    if(z > max)
+    {
+        max = z;
+    }
+
+    Console.WriteLine(max);
