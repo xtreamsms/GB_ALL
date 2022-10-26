82cdaad8a2c324b4a786904b3c06f8f774238107
«xtream» <xtreamsms@mail.ru>
2022-10-26 11:55:32 +0700

    2 ZAD s Parsingom 4isel v branche pppaaasssrrree

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = int.Parse(Console.ReadLine());
+int j = int.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+
+int max = i;
+
+    if(j > max)
+    {
+        max = j;
+    }
+    
+    if(z > max)
+    {
+        max = z;
+    }
+
+    Console.WriteLine(max);
